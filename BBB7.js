93afc5c031aa57ce625f65d6e655b68eeb772480
YaphetS <74091977+YaphetS0903@users.noreply.github.com>
2021-03-04 22:22:25 +0800

    Update BBB7.js

--- BBB7.js
+++ BBB7.js
@@ -11,7 +11,7 @@ if ($.isNode())
 	  
 
 
-CookieArr=[{"store":"appstore","tokenstr":"BA1895FA0065C86BD9ABEF734525263G1613994719","Connection":"keep-alive","Accept-Encoding":"gzip, deflate, br","version":"11","idfa":"45898213-188A-4055-B366-86CEDCE0D5B4","User-Agent":"BBB/133 CFNetwork/1209 Darwin/20.2.0","platform":"2","imei":"0EC01374-A9DF-4D49-ACF0-A539A479702F","Cookie":"PHPSESSID=pnddub5ur145rfnenu4ol94u65","Host":"bububao.duoshoutuan.com","Accept-Language":"zh-cn","Accept":"*/*","Content-Length":"0"}]
+CookieArr=[{"store":"appstore","tokenstr":"A9B06B8D8540907DDCE6EBFA3532761G1614867461","Connection":"keep-alive","Accept-Encoding":"gzip, deflate, br","version":"11","idfa":"B653A575-DD5F-4BF1-AB49-C2C4A086B0A3","User-Agent":"BBB/133 CFNetwork/1209 Darwin/20.2.0","platform":"2","imei":"8E643582-74A0-419C-995F-5E424D068DEF","Cookie":"PHPSESSID=cqfpvugtafscefc2vuamepnjb6","Host":"bububao.duoshoutuan.com","Accept-Language":"zh-cn","Accept":"*/*","Content-Length":"0"}]
 let dayjinbi=0;
 let lasttxTime=0;
 now = new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000);  
